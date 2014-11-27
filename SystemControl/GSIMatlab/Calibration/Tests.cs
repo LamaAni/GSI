c5e8305d235e59c7ffaa7b9e27130668e2d007be
Zav <Zav.Shotan@GMail.com>
2014-11-27 20:04:43 +0200

    First test of matlab native code. Run matlab in C#

---
+++ SystemControl/GSIMatlab/Calibration/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSI.Calibration
+{
+    public static class Tests
+    {
+        static Tests()
+        {
+        }
+
+        public static Tuple<double,double> FindRotationAndPixelSize()
+        {
+            //GSI.Matlab.Calibration calib = new Matlab.Calibration();
+            Matlab.Class1 c1 = new Matlab.Class1();
+            c1.FindRotationAndPixelSize();
+            return null;
+        }
+    }
+}
---
+++ SystemControl/TestMatalb/Form1.cs
@@ -0,0 +1,29 @@
+﻿using MathWorks.MATLAB.NET.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestMatalb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MathWorks.MATLAB.NET.Arrays.MWNumericArray arr =
+                new MathWorks.MATLAB.NET.Arrays.MWNumericArray();
+            GSI.Calibration.Tests.FindRotationAndPixelSize();
+        }
+    }
+}
