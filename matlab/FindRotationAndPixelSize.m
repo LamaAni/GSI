211e64b2f038ea6e8b4345e9537ee37127b8851e
Zav <Zav.Shotan@GMail.com>
2014-11-27 20:05:45 +0200

    Matlab source compiled dlls. The .Net compilation of matlab code.

--- matlab/FindRotationAndPixelSize.m
+++ matlab/FindRotationAndPixelSize.m
@@ -23,3 +23,10 @@ function [out]=fast_xcorr2(im1,im2)
     F12=(conj(F1)).*F2;
     out=abs(ifftshift(ifft2(F12)));
 end
+
+function [x_shift, y_shift]=find_shift(x_corr)
+    [y_shift, x_shift]=find(x_corr==max(x_corr(:)));
+
+    x_shift=x_shift-round(size(x_corr,2)./2)-1;
+    y_shift=y_shift-round(size(x_corr,1)./2)-1;
+end
