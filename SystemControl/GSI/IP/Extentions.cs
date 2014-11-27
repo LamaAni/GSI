ca9d206ee4ad9312d6467753ebc0f5a04447ca46
Zav Shotan <Zav.Shotan@GMail.com>
2014-11-27 14:20:29 -0500

    Created test code for matlab integration in the test api.

---
+++ SystemControl/GSI/IP/Extentions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GSI.IP
+{
+    public static class Extentions
+    {
+
+        /// <summary>
+        /// Returns all the bytes in the image.
+        /// </summary>
+        /// <param name="img"></param>
+        /// <returns></returns>
+        public static byte[] GetImageBytes(this Bitmap img, System.Drawing.Imaging.PixelFormat format
+            = System.Drawing.Imaging.PixelFormat.Format16bppGrayScale)
+        {
+            BitmapData data=img.LockBits(new Rectangle(new Point(0,0,),img.Size), System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                 format);
+            byte[] rslt=new byte[data.Width*data.Height*data.Stride];
+            Marshal.Copy(data.Scan0,rslt,0,data.Width*data.Height*data.Stride);
+            img.UnlockBits(data);
+            return rslt;
+        }
+    }
+}
--- SystemControl/TestMatalb/Form1.cs
+++ SystemControl/TestMatalb/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GSI.IP;
 
 namespace TestMatalb
 {
@@ -20,10 +21,19 @@ namespace TestMatalb
 
         private void button1_Click(object sender, EventArgs e)
         {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+            string first = dlg.FileName;
+            if (dlg.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+            string second = dlg.FileName;
+            Bitmap imga = (Bitmap)Bitmap.FromFile(first);
+            Bitmap imgb = (Bitmap)Bitmap.FromFile(second);
+            double angle=0, pixelSize=0;
 
-            MathWorks.MATLAB.NET.Arrays.MWNumericArray arr =
-                new MathWorks.MATLAB.NET.Arrays.MWNumericArray();
-            GSI.Calibration.SpatialRotation.FindRotationAndPixelSize();
+            GSI.Calibration.SpatialRotation.FindRotationAndPixelSize(imga.GetImageBytes().Cast<float>().ToArray(),
+                imgb.GetImageBytes().Cast<float>().ToArray(), imga.Width, 0, 100, out angle, out pixelSize);
         }
     }
 }
