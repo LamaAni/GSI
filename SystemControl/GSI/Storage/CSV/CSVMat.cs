7ab9513fd0f6151cdf05525ed2d22767b3351f2b
Zav <Zav.Shotan@GMail.com>
2014-12-04 01:22:27 +0200

    Added support for CSVMat and spectrum calibration info.

---
+++ SystemControl/GSI/Storage/CSV/CSVMat.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSI.Storage.CSV
+{
+    /// <summary>
+    /// Createds an in memory representation of a csv file.
+    /// Dose not require full lines. 
+    /// </summary>
+    public class CSVMat
+    {
+        public CSVMat(string source = "")
+        {
+            ParseCSV(source);
+        }
+
+        #region members
+
+        List<List<string>> m_data = new List<List<string>>();
+
+        /// <summary>
+        /// The total number of lines in the csv file.
+        /// </summary>
+        public int LineCount { get { return m_data.Count; } }
+
+        /// <summary>
+        /// Sets or gets the row and col values.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        public string this[int row, int col]
+        {
+            get
+            {
+                if (m_data.Count <= row || m_data[row].Count <= col)
+                {
+                    return null;
+                }
+                return m_data[row][col];
+            }
+            set
+            {
+                while (m_data.Count <= row)
+                    m_data.Add(new List<string>());
+
+                while (m_data[row].Count <= col)
+                    m_data[row].Add(null);
+
+                m_data[row][col] = value;
+            }
+        }
+
+        #endregion
+
+        #region convertsion methods
+
+        void ParseCSV(string source)
+        {
+            m_data.Clear();
+
+            // creating the csv 2d ma
+            source.Split('\n').Where(l => l.Trim().Length > 0).ToArray().Foreach(l=>{
+                m_data.Add(l.Split(',').ToList());
+            });
+        }
+
+        #endregion
+
+        #region string functions
+
+        /// <summary>
+        /// Display the csv as string.
+        /// </summary>
+        /// <returns></returns>
+        public string ToCSVString()
+        {
+            StringBuilder builder = new StringBuilder();
+            m_data.ForEach(l =>
+            {
+                builder.Append(string.Join(",", l.ToArray()));
+            });
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Create a new csvmatrix for the string source.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static CSVMat Parse(string source)
+        {
+            return new CSVMat(source);
+        }
+        #endregion
+    }
+}
---
+++ SystemControl/GSI/Storage/Spectrum/SpectrumCalibrationInfo.cs
@@ -0,0 +1,34 @@
+﻿using GSI.Storage.CSV;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSI.Storage.Spectrum
+{
+    /// <summary>
+    /// Holds information about a spectrum calibration, for several types of FFT zero fills.
+    /// </summary>
+    public class SpectrumCalibrationInfo
+    {
+        /// <summary>
+        /// Create a new calibration info from a csv file.
+        /// </summary>
+        /// <param name="source"></param>
+        public SpectrumCalibrationInfo(string source)
+            :this(new CSVMat(source))
+        {
+        }
+
+        /// <summary>
+        /// Create a new calibration info from a csv file.
+        /// </summary>
+        /// <param name="source"></param>
+        public SpectrumCalibrationInfo(CSVMat mat)
+        {
+        }
+
+        Dictionary<int, List<double>> paramsForCalibration = new Dictionary<int, List<double>>();
+    }
+}
